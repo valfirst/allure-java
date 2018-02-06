b2d95e6bc0d65c15193e0394c75e38f0a1d8aee1
bvo2002 <bvo2002@gmail.com>
2018-02-06 13:54:36 +0300

    add-pmd-corrections

--- allure-testng/src/test/java/io/qameta/allure/testng/samples/ParallelDataProviderSample.java
+++ allure-testng/src/test/java/io/qameta/allure/testng/samples/ParallelDataProviderSample.java
@@ -13,16 +13,16 @@ import java.util.List;
  */
 public class ParallelDataProviderSample {
 
-  @DataProvider(parallel = true)
-  Iterator<Integer[]> provide() {
-    List<Integer[]> ret = new ArrayList<>();
+  @DataProvider(name = "provideData", parallel = true)
+  Iterator<Object[]> provide() {
+    List<Object[]> ret = new ArrayList<>();
     for (int i = 0; i < 1000; i++) {
       ret.add(new Integer[]{i});
     }
     return ret.iterator();
   }
 
-  @Test(dataProvider = "provide", invocationCount = 2, threadPoolSize = 2)
+  @Test(dataProvider = "provideData", invocationCount = 2, threadPoolSize = 2)
   public void checkCME(Integer i) {
     Assert.assertNotNull(i);
   }
